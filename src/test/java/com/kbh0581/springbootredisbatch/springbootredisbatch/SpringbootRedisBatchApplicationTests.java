ba013a66aa59aa61251a43039138565a4e3c7d11
kbh0581 <kbh0581@gmail.com>
2021-09-16 11:34:12 +0900

    Initial commit

---
+++ src/test/java/com/kbh0581/springbootredisbatch/springbootredisbatch/SpringbootRedisBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbh0581.springbootredisbatch.springbootredisbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRedisBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

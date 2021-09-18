09777247de01942f74967cfdbf5dbb04e62a9a81
kbh0581 <kbh0581@gmail.com>
2021-09-18 23:28:09 +0900

    멀티 모듈 전환

---
+++ spring-redis-batch-front/src/main/java/front/controller/PageController.java
@@ -0,0 +1,13 @@
+package front.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/")
+    public String main(){
+        return "main";
+    }
+}
